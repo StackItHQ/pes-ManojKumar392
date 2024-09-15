05ebe2d0980d362c03097fc56f00f41a0421b284
Manoj Kumar R <manoj2003r@gmail.com>
2024-09-15 09:28:42 +0530

    Add sheets app and configure PostgreSQL database

--- google_sheet_sync/google_sheet_sync/settings.py
+++ google_sheet_sync/google_sheet_sync/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sheets',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'google_sheet_sync.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myprojectdb',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'FireandFighting392',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
